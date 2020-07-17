b2949f4cca96c6621b3c862edd1face741dfa77f
Vasiliy <kkolyasev1228@gmail.com>
2020-07-17 18:21:26 +0300

    new state

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'PostController@index')->name('index');
 Route::get('post/', 'PostController@index')->name('post.index');
 Route::get('post/create', 'PostController@create')->name('post.create');
+Route::post('post/', 'PostController@store')->name('post.store');
